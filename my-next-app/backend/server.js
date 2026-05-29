2f5e627f6ed54a8c4d1afc6bcb0605f299ddd4f3
Heet <iamheetchokshi@gmail.com>
2026-05-29 14:31:33 +0530

    bugs

--- my-next-app/backend/server.js
+++ my-next-app/backend/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/CryptoNexus")
+mongoose.connect("mongodb+srv://iamheetchokshi_db_user:S6IjhHTtO7rLUw2a@cryptonexus.fx9fte6.mongodb.net/?appName=Cryptonexus")
 .then(() => console.log("Connected to MongoDB"))
 .catch((err) => console.log("Failed to connect to MongoDB", err));
 
